af7eae771ee029e5ac5f2330412fc28d6b4c1a11
Xinxia <985025074@qq.com>
2024-10-31 13:06:09 +0800

    fix function

--- rebuilt_muduo/basic_tools/FormatTools.cpp
+++ rebuilt_muduo/basic_tools/FormatTools.cpp
@@ -1,16 +1,16 @@
 #include "FormatTools.h"
 namespace syc{
     namespace details{
-inline std::string_view get_filename(std::string_view filename){
+std::string_view get_filename(std::string_view filename){
     auto loc = filename.rfind('/');
     return std::string_view(filename.begin()+loc+1,filename.end());
 }
-inline std::string get_gmt_time(){
+std::string get_gmt_time(){
     std::time_t currentTime = std::time(nullptr);
     std::tm* gmtTime = std::gmtime(&currentTime);
     std::string gmtTimeStr(std::asctime(gmtTime));
     return gmtTimeStr;
-
 }   
+
     };
 };
